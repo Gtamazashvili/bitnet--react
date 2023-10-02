9a362c8c693cee8f015df106350a59a74aeb12ae
Giorgi Tamazashvili <118844834+Gtamazashvili@users.noreply.github.com>
2023-10-02 16:53:39 +0400

    Update About.tsx

--- pages/About/About.tsx
+++ pages/About/About.tsx
@@ -1,11 +1,11 @@
 import "./About.css";
-import { AboutAndPress } from "../../components/AboutAndPress"; // Adjust the import to named import
+import { AboutAndPress } from "../../components/AboutAndPress"; 
 import Press from "../../components/Press";
 import WhoWeAre from "../../components/WhoWeAre";
 import { useState } from "react";
 
 export default function About() {
-  const [selectedTab, setSelectedTab] = useState("whoWeAre"); // Initialize with "whoWeAre" or any default value
+  const [selectedTab, setSelectedTab] = useState("whoWeAre"); 
 
   return (
     <>
